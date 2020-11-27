0e6dfb44c01bab5fc0244da0c45502073d3d3210
Vaclav Svejcar <vaclav.svejcar@gmail.com>
2020-11-27 15:54:31 +0100

    Reexport modules in 'Data.VCS.Ignore', disable Windows build

--- src/Data/VCS/Ignore.hs
+++ src/Data/VCS/Ignore.hs
@@ -1,6 +1,6 @@
 {-|
 Module      : Data.VCS.Ignore
-Description : !!! INSERT MODULE SHORT DESCRIPTION !!!
+Description : Reexported modules for convenience
 Copyright   : (c) 2020 Vaclav Svejcar
 License     : BSD-3-Clause
 Maintainer  : vaclav.svejcar@gmail.com
@@ -10,4 +10,27 @@ Portability : POSIX
 !!! INSERT MODULE LONG DESCRIPTION !!!
 -}
 
-module Data.VCS.Ignore where
+module Data.VCS.Ignore
+  ( -- Working with ignored files
+    findRepo
+  , listRepo
+  , walkRepo
+    -- * Repo /type class/
+  , Repo(..)
+  , RepoError(..)
+    -- * /GIT/ implementation
+  , Git(..)
+    -- * Common data types
+  , VCSIgnoreError(..)
+  )
+where
+
+import           Data.VCS.Ignore.Core           ( findRepo
+                                                , listRepo
+                                                , walkRepo
+                                                )
+import           Data.VCS.Ignore.Repo           ( Repo(..)
+                                                , RepoError(..)
+                                                )
+import           Data.VCS.Ignore.Repo.Git       ( Git(..) )
+import           Data.VCS.Ignore.Types          ( VCSIgnoreError(..) )
